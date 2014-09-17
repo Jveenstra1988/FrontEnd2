c86eca700b61bbf81a6ccbfa01db295a6b064783
Jveenstra1988 <jveenstra1988@gmail.com>
2014-09-17 16:16:45 +0200

    router toegevoegd

--- WebApp/static/js/app.js
+++ WebApp/static/js/app.js
@@ -1,3 +1,36 @@
-var MOVIEDB = {
-  
-}
+
+
+//namespace
+var webApp = webApp || {};
+
+//self executing anonymous function
+(function(){
+	
+	//controller object (literal)
+	webApp.controller = {
+		init: function(){
+			webApp.router.init()
+		}
+	}
+	
+	//router object (literal)
+	webApp.router = {
+		init: function(){
+			routie({
+				'about': function() {
+					console.log('You clicked about')
+				},
+				'movies': function() {
+					console.log('You clicked movies')
+				}
+			})
+		}
+	}
+	
+	//execute
+	webApp.controller.init()
+	
+//end of self executing anonymous function
+})();
+
+
